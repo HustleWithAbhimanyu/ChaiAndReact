1ae135b31d059340df319f70488caba172a59f17
HustleWithAbhimanyu <hustlewithabhimanyu@gmail.com>
2023-11-17 23:28:05 +0530

    Adding negative Check

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -7,22 +7,27 @@ function App() {
   let [counter, setCount] = useState(1)
 
   const addValue =()=>{
+
+    if(counter > 0 && counter < 20){
+      setCount(counter+1);
+    }
     
-    setCount(counter+1);
+    // setCount(counter+1);
     //console.log("clicked",+counter);
   }
   const DecValue =()=>{
-    
-    counter=counter-1;
-    setCount(counter);
+
+    if(counter>-1 ){
+      setCount(counter--);
+    }
     //console.log("clicked for Remove",+counter);
   }
 
   return (
     <>
-       <h1>Chai aur react</h1>
+       <h1>Counter Project</h1>
        <h2>Counter value :{counter}</h2>
-       <button onClick={addValue}> Add value :{counter}</button><br></br>
+       <button onClick={addValue}> Add value :{counter}</button><br/><br/>
        <button onClick={DecValue}>Decrease value :{counter}</button>
     </>
   )
